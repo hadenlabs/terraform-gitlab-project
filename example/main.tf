d132b5a0332b45d164c90b7c7c214bd8fa0efc2a
@slovacus <slovacus@gmail.com>
2021-03-19 05:14:45 -0500

    build: generate version minor (0.1.0)

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ module "main" {
   providers = {
     gitlab = gitlab
   }
-  source      = "git://github.com/hadenlabs/terraform-gitlab-repository.git?ref=0.0.0"
+  source      = "git://github.com/hadenlabs/terraform-gitlab-repository.git?ref=0.1.0"
   name        = var.repository_name
   description = var.repository_description
   visibility  = "public"
