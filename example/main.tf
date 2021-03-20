c738815089ad3d4cb707e5d9646cce203bfdaa1d
@slovacus <slovacus@gmail.com>
2021-03-20 10:36:53 -0500

    fix: version in examples (#9)

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ module "main" {
   providers = {
     gitlab = gitlab
   }
-  source      = "git://github.com/hadenlabs/terraform-gitlab-project.git?ref=0.0.0"
+  source      = "git://github.com/hadenlabs/terraform-gitlab-project.git?ref=0.1.0"
   name        = var.project_name
   description = var.project_description
   visibility  = "public"
