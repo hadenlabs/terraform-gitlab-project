ac7923d55b250dc27727c1d444414369b20f917a
luismayta <luis@hadenlabs.com>
2021-09-28 03:03:06 -0500

    refactor: dependencies versions standard (#23)

--- versions.tf
+++ versions.tf
@@ -1,14 +1,10 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 0.13"
+
   required_providers {
     gitlab = {
       source  = "gitlabhq/gitlab"
       version = ">=3.5.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = ">=1.3.0"
     }
   }
 }
